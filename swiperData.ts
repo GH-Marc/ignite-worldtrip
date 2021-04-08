ad6e20de470aa9549d06da3e7d24d7283dbb65c1
Marc <marcelopgm65@gmail.com>
2021-04-08 19:21:33 -0300

    :sparkles: feat: Initial data to Swiper/Continents

---
+++ swiperData.ts
@@ -0,0 +1,22 @@
+export const swiperData = {
+  continents: [
+    {
+      id: 1,
+      image: "/images/swiper-images/swiper-europe.png",
+      name: "Europa",
+      description: "O continente mais antigo.",
+    },
+    {
+      id: 2,
+      image: "/images/swiper-images/swiper-europe.png",
+      name: "Europa",
+      description: "O continente mais antigo.",
+    },
+    {
+      id: 3,
+      image: "/images/swiper-images/swiper-europe.png",
+      name: "Europa",
+      description: "O continente mais antigo.",
+    },
+  ],
+};
