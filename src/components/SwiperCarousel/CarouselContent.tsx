1a91c7d599e873df3e288d68bc5cc477c4efe037
Marc <marcelopgm65@gmail.com>
2021-04-08 19:24:41 -0300

    :sparkles: feat: Carousel Content component

---
+++ src/components/SwiperCarousel/CarouselContent.tsx
@@ -0,0 +1,39 @@
+import { Flex, Text } from "@chakra-ui/react";
+import Link from "next/link";
+
+interface CarouselContentProps {
+  id: number;
+  image: string;
+  name: string;
+  description: string;
+}
+
+export function CarouselContent({
+  id,
+  image,
+  name,
+  description,
+}: CarouselContentProps) {
+  return (
+    <Link href={`continent/${id}`}>
+      <Flex
+        backgroundImage={`url(${image})`}
+        backgroundRepeat="no-repeat"
+        backgroundPosition="center"
+        backgroundSize="cover"
+        w="100%"
+        cursor="pointer"
+        h={["250px", "450px"]}
+      >
+        <Flex justify="center" align="center" direction="column" w="100%">
+          <Text fontSize="3rem" fontWeight="700" color="white.600">
+            {name}
+          </Text>
+          <Text fontSize="1.5rem" fontWeight="700" color="gray.300">
+            {description}
+          </Text>
+        </Flex>
+      </Flex>
+    </Link>
+  );
+}
