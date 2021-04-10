f7bdc318c9f59e44f4bcb4370a7478a4ac5cb1ec
Marc <marcelopgm65@gmail.com>
2021-04-09 23:36:54 -0300

    :wrench: chore: Adds back button to pages

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,16 +1,34 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Box, Image } from "@chakra-ui/react";
+import Link from "next/link";
+
+import { useRouter } from "next/router";
 
 export function Header() {
+  const { asPath } = useRouter();
+
   return (
-    <Flex
-      as="header"
-      w="100%"
-      maxWidth={1440}
-      mx="auto"
-      align="center"
-      justify="center"
-    >
-      <Image src="/images/logo.svg" alt="Worldtrip" w="11.5rem" m="1.7rem 0" />
-    </Flex>
+    <Link href="/">
+      <Flex
+        as="header"
+        w="100%"
+        maxWidth={1440}
+        mx="auto"
+        align="center"
+        justify="center"
+        position="relative"
+      >
+        {asPath !== "/" && (
+          <Box position="absolute" left="8.75rem">
+            <Image src="/images/arrowLeft.svg" cursor="pointer" />
+          </Box>
+        )}
+        <Image
+          src="/images/logo.svg"
+          alt="Worldtrip"
+          w="11.5rem"
+          m="1.7rem 0"
+        />
+      </Flex>
+    </Link>
   );
 }
