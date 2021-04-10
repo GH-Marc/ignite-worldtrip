ef081994b3313a63ed98218c56c845fefecf2f48
Marc <marcelopgm65@gmail.com>
2021-04-09 23:35:51 -0300

    :wrench: chore: Dynamic banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,14 +1,49 @@
-import { Flex, Image } from "@chakra-ui/react";
+import { Flex, Image, Text } from "@chakra-ui/react";
 
 import { BannerContent } from "./BannerContent";
 
-export function Banner() {
+interface BannerProps {
+  src?: string;
+  headerContinent?: string;
+  heightHeaderContinent?: string;
+}
+
+export function Banner({
+  src,
+  headerContinent,
+  heightHeaderContinent,
+}: BannerProps) {
   return (
-    <Flex w="100%" h="335" pos="relative" justify="center">
-      <Image src="/images/background.png" alt="Background" w="100%" />
+    <Flex
+      w="100%"
+      h={heightHeaderContinent ? heightHeaderContinent : "335"}
+      pos="relative"
+      justify="center"
+    >
+      <Image
+        src={src ? src : "/images/background.png"}
+        alt="Background"
+        w="100%"
+      />
       <Flex pos="absolute" w="100%" maxW="1440px" h="100%" direction="column">
-        <Flex align="center" pos="relative" m="auto 0">
-          <BannerContent />
+        <Flex
+          align="center"
+          pos={heightHeaderContinent ? "absolute" : "relative"}
+          m="auto 0"
+          top={heightHeaderContinent ? "369" : "0"}
+        >
+          {headerContinent ? (
+            <Text
+              fontWeight="600"
+              color="white.600"
+              fontSize="2.25rem"
+              ml="8.75rem"
+            >
+              {headerContinent}
+            </Text>
+          ) : (
+            <BannerContent />
+          )}
         </Flex>
       </Flex>
     </Flex>
