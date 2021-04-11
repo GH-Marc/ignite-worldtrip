141a7935cae551644f86b8baf199d8e84ad8ecd7
Marc <marcelopgm65@gmail.com>
2021-04-11 18:31:12 -0300

    :wrench: chore: Verification if is antardida path

--- src/components/ContinentDetails/index.tsx
+++ src/components/ContinentDetails/index.tsx
@@ -1,5 +1,7 @@
 import { Text, Flex, Heading, Image, Tooltip } from "@chakra-ui/react";
 
+import { useRouter } from "next/router";
+
 interface ContinentDetailsProps {
   amountDetail: number;
   legend: string;
@@ -11,46 +13,54 @@ export function ContinentDetails({
   legend,
   hasTooltip = false,
 }: ContinentDetailsProps) {
-  return (
-    <Flex
-      direction="column"
-      align={["flex-start", "center"]}
-      pl={["0", "70px"]}
-    >
-      <Heading
-        fontWeight="600"
-        fontSize="3rem"
-        color="yellow.900"
-        lineHeight="9"
-      >
-        {amountDetail}
-      </Heading>
-      {hasTooltip ? (
-        <Flex align="center">
-          <Text fontWeight="600" fontSize="1.5rem">
-            {legend}
-          </Text>
+  const { asPath } = useRouter();
 
-          <Tooltip
-            label="Em amarelo são as mais visitadas"
-            aria-label="Tooltip"
-            bg="light.800"
-            borderRadius="4px"
-          >
-            <Image
-              src="/images/info.svg"
-              w="16px"
-              h="16px"
-              ml="5px"
-              color="light.800"
-            />
-          </Tooltip>
-        </Flex>
+  return (
+    <>
+      {asPath === "/continent/antardida" ? (
+        ""
       ) : (
-        <Text fontWeight="600" fontSize="1.5rem">
-          {legend}
-        </Text>
+        <Flex
+          direction="column"
+          align={["flex-start", "center"]}
+          pl={["0", "70px"]}
+        >
+          <Heading
+            fontWeight="600"
+            fontSize="3rem"
+            color="yellow.900"
+            lineHeight="9"
+          >
+            {amountDetail}
+          </Heading>
+          {hasTooltip ? (
+            <Flex align="center">
+              <Text fontWeight="600" fontSize="1.5rem">
+                {legend}
+              </Text>
+
+              <Tooltip
+                label="Em amarelo são as mais visitadas"
+                aria-label="Tooltip"
+                bg="light.800"
+                borderRadius="4px"
+              >
+                <Image
+                  src="/images/info.svg"
+                  w="16px"
+                  h="16px"
+                  ml="5px"
+                  color="light.800"
+                />
+              </Tooltip>
+            </Flex>
+          ) : (
+            <Text fontWeight="600" fontSize="1.5rem">
+              {legend}
+            </Text>
+          )}
+        </Flex>
       )}
-    </Flex>
+    </>
   );
 }
