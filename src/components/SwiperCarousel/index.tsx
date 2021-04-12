27426da1fda503ade1a708d2d65b69da302a480b
Marc <marcelopgm65@gmail.com>
2021-04-12 10:04:20 -0300

    :iphone: feat: Responsive swiper & other funcs

--- src/components/SwiperCarousel/index.tsx
+++ src/components/SwiperCarousel/index.tsx
@@ -1,10 +1,16 @@
 import { Flex } from "@chakra-ui/react";
 import { Swiper, SwiperSlide } from "swiper/react";
 
-import SwiperCore, { Navigation, Pagination, Autoplay } from "swiper";
+import SwiperCore, {
+  Navigation,
+  Pagination,
+  Scrollbar,
+  A11y,
+  Autoplay,
+} from "swiper";
 import { CarouselContent } from "./CarouselContent";
 
-SwiperCore.use([Navigation, Pagination, Autoplay]);
+SwiperCore.use([Navigation, Pagination, Scrollbar, A11y, Autoplay]);
 
 interface Continent {
   slug: string;
@@ -19,18 +25,23 @@ interface SwiperCarouselProps {
 
 export function SwiperCarousel({ continents }: SwiperCarouselProps) {
   return (
-    <Flex w="90%" align="center" justify="center" mb="2.5rem">
+    <Flex
+      w="90%"
+      h={["250px", "450px"]}
+      maxW="1240px"
+      mx="auto"
+      mb={["5", "10"]}
+    >
       <Swiper
-        wrapperTag="ul"
-        spaceBetween={0}
         slidesPerView={1}
         navigation
         pagination={{ clickable: true }}
         loop
         autoplay={{ delay: 3000 }}
+        style={{ width: "100%", flex: "1" }}
       >
         {continents.map((continent) => (
-          <SwiperSlide tag="li" key={`slide-${continent.slug}`}>
+          <SwiperSlide key={`slide-${continent.slug}`}>
             <CarouselContent
               slug={continent.slug}
               image={continent.swiperImage}
