e8ab045ff31ca9d2a2daf53a175699885140f6a4
Marc <marcelopgm65@gmail.com>
2021-04-12 09:55:09 -0300

    :iphone: Responsive banner

--- src/components/Banner/index.tsx
+++ src/components/Banner/index.tsx
@@ -1,50 +1,52 @@
-import { Flex, Image, Text } from "@chakra-ui/react";
+import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
 
-import { BannerContent } from "./BannerContent";
-
-interface BannerProps {
-  src?: string;
-  headerContinent?: string;
-  heightHeaderContinent?: string;
-}
-
-export function Banner({
-  src,
-  headerContinent,
-  heightHeaderContinent,
-}: BannerProps) {
+export function Banner() {
   return (
     <Flex
       w="100%"
-      h={heightHeaderContinent ? heightHeaderContinent : "335"}
+      h={["163px", "250px", "250px", "335px"]}
+      bgImage="url('/images/background.png')"
+      bgPosition={["100% 20%", "100% 20%", "100% 30%"]}
+      bgRepeat="no-repeat"
+      bgSize="cover"
       pos="relative"
       justify="center"
     >
-      <Image
-        src={src ? src : "/images/background.png"}
-        alt="Background"
+      <Flex
+        justify={["center", "space-between"]}
+        align="center"
         w="100%"
-      />
-      <Flex pos="absolute" w="100%" maxW="1440px" h="100%" direction="column">
-        <Flex
-          align="center"
-          pos={heightHeaderContinent ? "absolute" : "relative"}
-          m="auto 0"
-          top={heightHeaderContinent ? "369" : "0"}
-        >
-          {headerContinent ? (
-            <Text
-              fontWeight="600"
-              color="white.600"
-              fontSize="3rem"
-              ml="8.75rem"
-            >
-              {headerContinent}
-            </Text>
-          ) : (
-            <BannerContent />
-          )}
-        </Flex>
+        maxW="1440px"
+        mx="auto"
+        px={["4", "10", "15", "20", "36"]}
+      >
+        <Box>
+          <Heading
+            color="gray.100"
+            fontWeight="500"
+            fontSize={["xl", "2xl", "2xl", "2xl", "4xl"]}
+          >
+            6 continentes,
+            <br />
+            infinitas possibilidades.
+          </Heading>
+          <Text
+            color="gray.300"
+            mt="5"
+            fontSize={["0.8rem", "xl"]}
+            maxW={["100%", "100%", "100%", "550px"]}
+          >
+            Chegou a hora de tirar do papel a viagem que você sempre sonhou.
+          </Text>
+        </Box>
+        <Image
+          src="/images/airplane.svg"
+          alt="Avião"
+          display={["none", "none", "block"]}
+          w={["300px", "300px", "300px", "430px"]}
+          transform="translateY(48px)"
+          ml="8"
+        />
       </Flex>
     </Flex>
   );
