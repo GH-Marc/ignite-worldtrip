09a35c09d01e46b836b1fcb8ac44967faae8157b
Marc <marcelopgm65@gmail.com>
2021-04-13 14:30:06 -0300

    :bug: chore: Correction to favicon in routes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -5,8 +5,9 @@ export default class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="shortcut icon" href="favicon.png" type="image/png" />
           <link rel="preconnect" href="https://fonts.gstatic.com" />
+          <link rel="shortcut icon" href="/favicon.png" type="image/png" />
+
           <meta charSet="utf-8" />
           <link
             href="https://fonts.googleapis.com/css2?family=Poppins:wght@400;500;700&display=swap"
