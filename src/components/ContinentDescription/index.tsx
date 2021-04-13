0b49f75eb7402087afa2e8dd6ed9100d0e86cb96
Marc <marcelopgm65@gmail.com>
2021-04-13 12:23:01 -0300

    :iphone: chore: Responsive description

--- src/components/ContinentDescription/index.tsx
+++ src/components/ContinentDescription/index.tsx
@@ -1,27 +1,32 @@
-import { Flex, Text } from "@chakra-ui/react";
-
+import { Grid, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
-interface ContinentInfosProps {
-  continentDescription: string;
-}
+import { ContinentDetails } from "./ContinentDetails";
+import { ContinentProps } from "../../pages/continent/[slug]";
 
-export function ContinentDescription({
-  continentDescription,
-}: ContinentInfosProps) {
+export function ContinentDescription({ continent }: ContinentProps) {
   const { asPath } = useRouter();
 
   return (
-    <Flex>
+    <Grid
+      templateColumns={["1fr", "1fr", "1fr 1fr", "1.2fr 1fr"]}
+      gap={[5, 10, 16, 20]}
+      my={["8", "20"]}
+    >
       {asPath === "/continent/antardida" ? (
-        <Text fontSize="1.5rem" align="justify">
-          {continentDescription}
+        <Text
+          fontSize={["lg", "xl", "xl", "2xl"]}
+          textAlign="justify"
+          maxW="600px"
+        >
+          {continent.continentDescription}
         </Text>
       ) : (
-        <Text fontSize="1.5rem" align="justify" maxW="600px">
-          {continentDescription}
+        <Text fontSize={["lg", "xl", "xl", "2xl"]} textAlign="justify">
+          {continent.continentDescription}
         </Text>
       )}
-    </Flex>
+      <ContinentDetails continent={continent} />
+    </Grid>
   );
 }
