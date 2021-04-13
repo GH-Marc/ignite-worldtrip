a2f46f576407998a6b05a4cc99a7361abd934854
Marc <marcelopgm65@gmail.com>
2021-04-13 14:27:42 -0300

    :wrench: chore: Verification to antardida id

--- src/components/ContinentDescription/index.tsx
+++ src/components/ContinentDescription/index.tsx
@@ -2,30 +2,22 @@ import { Grid, Text } from "@chakra-ui/react";
 import { useRouter } from "next/router";
 
 import { ContinentDetails } from "./ContinentDetails";
-import { ContinentProps } from "../../pages/continent/[slug]";
+import { ContinentProps } from "../../pages/continent/[id]";
 
 export function ContinentDescription({ continent }: ContinentProps) {
   const { asPath } = useRouter();
 
   return (
     <Grid
-      templateColumns={["1fr", "1fr", "1fr 1fr", "1.2fr 1fr"]}
+      templateColumns={
+        asPath !== "/continent/6" && ["1fr", "1fr", "1fr 1fr", "1.2fr 1fr"]
+      }
       gap={[5, 10, 16, 20]}
       my={["8", "20"]}
     >
-      {asPath === "/continent/antardida" ? (
-        <Text
-          fontSize={["lg", "xl", "xl", "2xl"]}
-          textAlign="justify"
-          maxW="600px"
-        >
-          {continent.continentDescription}
-        </Text>
-      ) : (
-        <Text fontSize={["lg", "xl", "xl", "2xl"]} textAlign="justify">
-          {continent.continentDescription}
-        </Text>
-      )}
+      <Text fontSize={["lg", "xl", "xl", "2xl"]} textAlign="justify">
+        {continent.continentDescription}
+      </Text>
       <ContinentDetails continent={continent} />
     </Grid>
   );
