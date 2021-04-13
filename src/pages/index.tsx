480ac65087722e61455e60825f42b07204b2a590
Marc <marcelopgm65@gmail.com>
2021-04-13 12:26:22 -0300

    :wrench: chore: getStaticProps function

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,4 +1,5 @@
 import { Divider, Flex, Heading } from "@chakra-ui/react";
+import { GetStaticProps } from "next";
 import Head from "next/head";
 
 import { Banner } from "../components/Banner";
@@ -6,11 +7,18 @@ import { Header } from "../components/Header";
 import { SwiperCarousel } from "../components/SwiperCarousel";
 import { Travel } from "../components/Travel";
 
-import database from "../../database.json";
+import { api } from "../services/api";
 
-export default function Home() {
-  const { continents } = database;
+interface HomeProps {
+  continents: {
+    slug: string;
+    name: string;
+    description: string;
+    swiperImage: string;
+  }[];
+}
 
+export default function Home({ continents }: HomeProps) {
   return (
     <Flex direction="column">
       <Head>
@@ -41,3 +49,15 @@ export default function Home() {
     </Flex>
   );
 }
+
+export const getStaticProps: GetStaticProps = async () => {
+  const response = await api.get("/continents");
+
+  const continents = response.data;
+
+  return {
+    props: {
+      continents,
+    },
+  };
+};
